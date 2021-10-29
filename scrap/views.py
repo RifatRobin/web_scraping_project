787ba579bd00a613fc8f0379cbf3cdaa071108a3
Rifat <rifatitna@gmail.com>
2021-10-30 03:39:22 +0600

    Modified the result table of 'index.html' and Modified 'views.py'

--- scrap/views.py
+++ scrap/views.py
@@ -10,13 +10,13 @@ import urllib
 # created view for the scraping process of without tag attribute
 def data_input(request):
     extracted_data = []
+    # the web address link will store in this variable
+    web_address = request.POST.get('web_link', 'default')
+
+    # the tag will store in this variable
+    scrap_tag = request.POST.get('tag_code', 'default')
+
     if request.method == 'POST':
-        # the web address link will store in this variable
-        web_address = request.POST.get('web_link', 'default')
-
-        # the tag will store in this variable
-        scrap_tag = request.POST.get('tag_code', 'default')
-
         # code for the scraping process
         with urllib.request.urlopen(web_address) as response:
             html = response.read()
@@ -24,19 +24,20 @@ def data_input(request):
         for data in scrapval.find_all(scrap_tag):
             desired_values = data.get_text()
             extracted_data.append(desired_values)
-    return render(request, "scrap/index.html", {'extracted_data': extracted_data})
+    return render(request, "scrap/index.html", {'extracted_data': extracted_data, 'web_address': web_address, 'scrap_tag': scrap_tag})
 
 
 # created view for the scraping process for tag attribute
 def with_attributes(request):
     extracted_data = []
-    if request.method == 'POST':
-        # the web address link will store in this variable
-        web_address = request.POST.get('web_link', 'default')
+    # the web address link will store in this variable
+    web_address = request.POST.get('web_link', 'default')
 
-        # the tag will store in this variable
-        scrap_tag = request.POST.get('tag_code', 'default')
-        tag_attribute = request.POST.get('attribute', 'default')
+    # the tag will store in this variable
+    scrap_tag = request.POST.get('tag_code', 'default')
+    tag_attribute = request.POST.get('attribute', 'default')
+
+    if request.method == 'POST':
 
         # code for the scraping process
         with urllib.request.urlopen(web_address) as response:
@@ -47,4 +48,4 @@ def with_attributes(request):
             desired_values = data.get(tag_attribute)
             extracted_data.append(desired_values)
 
-    return render(request, "scrap/index.html", {'extracted_data': extracted_data})
+    return render(request, "scrap/index.html", {'extracted_data': extracted_data, 'web_address': web_address, 'scrap_tag': scrap_tag, 'tag_attribute': tag_attribute})
