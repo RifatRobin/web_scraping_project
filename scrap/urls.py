7fc03d09b142aba1ed00b3f570bdfccb5271c41b
Rifat <rifatitna@gmail.com>
2021-10-30 03:15:51 +0600

    Added result table in 'index.html' file and Modified 'views.py','urls.py'

--- scrap/urls.py
+++ scrap/urls.py
@@ -1,10 +1,7 @@
-
-from django.contrib import admin
 from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.data_input, name='data_input'),
     path('with_attributes', views.with_attributes, name='with_attributes'),
 ]
--- scrap/views.py
+++ scrap/views.py
@@ -10,7 +10,6 @@ import urllib
 # created view for the scraping process of without tag attribute
 def data_input(request):
     extracted_data = []
-
     if request.method == 'POST':
         # the web address link will store in this variable
         web_address = request.POST.get('web_link', 'default')
@@ -25,16 +24,12 @@ def data_input(request):
         for data in scrapval.find_all(scrap_tag):
             desired_values = data.get_text()
             extracted_data.append(desired_values)
-        print(extracted_data)
-
     return render(request, "scrap/index.html", {'extracted_data': extracted_data})
-
 
 
 # created view for the scraping process for tag attribute
 def with_attributes(request):
     extracted_data = []
-
     if request.method == 'POST':
         # the web address link will store in this variable
         web_address = request.POST.get('web_link', 'default')
@@ -51,6 +46,5 @@ def with_attributes(request):
         for data in scrapval.find_all(scrap_tag):
             desired_values = data.get(tag_attribute)
             extracted_data.append(desired_values)
-        print(extracted_data)
 
     return render(request, "scrap/index.html", {'extracted_data': extracted_data})
