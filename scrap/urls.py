a034108b1c6c08eeacc843721dc4a8e2503c7917
Rifat <rifatitna@gmail.com>
2021-10-30 02:35:40 +0600

    created two fuctions in 'views.py'. One is for tags without attribute and another is for the tags that have attributes.Then modified 'urls.py'. Then modified the 'index.html' to show the result in the page and added to froms to take inputs.

--- scrap/urls.py
+++ scrap/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.data_input,name='data_input'),
+    path('', views.data_input, name='data_input'),
+    path('with_attributes', views.with_attributes, name='with_attributes'),
 ]
--- scrap/views.py
+++ scrap/views.py
@@ -1,28 +1,56 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
-#to use beautifulsoup4 for scraping we need to have these libraries in our code
+# to use beautifulsoup4 for scraping we need to have these libraries in our code
 import bs4
 from bs4 import BeautifulSoup as bs
 import urllib
 
 
-# created view for the scraping process
+# created view for the scraping process of without tag attribute
 def data_input(request):
-    # the web address link will store in this variable
-    web_address = request.POST.get('web_link', 'default')
-    print(web_address)  # for checking purpose , will remove it later
-
-    # the tag will store in this variable
-    scrap_tag = request.POST.get('tag_code', 'default')
-    print(scrap_tag)
-
+    extracted_data = []
 
     if request.method == 'POST':
+        # the web address link will store in this variable
+        web_address = request.POST.get('web_link', 'default')
+
+        # the tag will store in this variable
+        scrap_tag = request.POST.get('tag_code', 'default')
+
+        # code for the scraping process
         with urllib.request.urlopen(web_address) as response:
             html = response.read()
-        text = str(html)
-        soup = bs4.BeautifulSoup(text, 'html.parser')
-        print(soup.find_all(scrap_tag))
+        scrapval = bs4.BeautifulSoup(html, 'html.parser')
+        for data in scrapval.find_all(scrap_tag):
+            desired_values = data.get_text()
+            extracted_data.append(desired_values)
+        print(extracted_data)
 
-    return render(request, "scrap/index.html")
+    return render(request, "scrap/index.html", {'extracted_data': extracted_data})
+
+
+
+# created view for the scraping process for tag attribute
+def with_attributes(request):
+    extracted_data = []
+
+    if request.method == 'POST':
+        # the web address link will store in this variable
+        web_address = request.POST.get('web_link', 'default')
+
+        # the tag will store in this variable
+        scrap_tag = request.POST.get('tag_code', 'default')
+        tag_attribute = request.POST.get('attribute', 'default')
+
+        # code for the scraping process
+        with urllib.request.urlopen(web_address) as response:
+            html = response.read()
+        scrapval = bs4.BeautifulSoup(html, 'html.parser')
+
+        for data in scrapval.find_all(scrap_tag):
+            desired_values = data.get(tag_attribute)
+            extracted_data.append(desired_values)
+        print(extracted_data)
+
+    return render(request, "scrap/index.html", {'extracted_data': extracted_data})
